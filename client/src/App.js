874e257b4a37493e08206e570a82bf8d3ed055ae
kupharmd05 <kupharmd05@gmail.com>
2019-04-28 19:40:50 -0500

    A bunch of crap/aka okta

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-import Login from "./components/login/login"
 import CreateLogin from "./components/login/create-login"
 import './App.css';
 import Camera from "./components/camera/camera"
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import VerifyInfo from './components/verify/verify-info';
+import Navbar from './components/layout/Navbar';
+import {Security, SecureRoute, ImplicitCallback} from '@okta/okta-react';
+import Login from './components/auth/login';
+import Home from './components/pages/Home';
+import registrationForm from './components/auth/registrationForm';
 
-
+function onAuthRequired({ history }) {
+  history.push('/login')
+}
 
 class App extends Component {
 
     render() {
     return (
+      
       <Router>
+        <Security issuer='https://dev-680655.okta.com/oauth2/default'
+                  client_id='0oaiiphavOO8bpeh2356'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired} >
         <div>
-          <div className="login-page" style={{ height: '100vh' }}>
-
+         
+          <Navbar />
+            <div className="container">
+              <Route exact path="/" component={Home}/>
+              <Route path="/create" component={CreateLogin}/>
+              <SecureRoute path="/camera" component={Camera}/>
+              <Route path='/login' render={() => <Login baseUrl='https://dev-680655.okta.com' />} />
+              <Route path='/register' component={registrationForm} />
+              <Route path='/implicit/callback' component={ImplicitCallback} />
+              
+            </div>
             
-            <Route exact path="/" component={Login}/>
-            <Route path="/create" component={CreateLogin}/>
-            <Route path="/camera" component={Camera}/>
-            <Route path="/verify" component={VerifyInfo}/>
-          </div>
+          
         </div>
-
+        </Security>
       </Router>
      
       
--- models/contact.js
+++ models/contact.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const contactSchema = new Schema({
-  name: { type: String, required: true },
-  username: {type: String, required: true},
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
-  password: {type: String, require: true},
+  login: { type: String, required: true },
+  
   
 });
 
