6c9c73f891935d793d3c0c4f3681acc57f5ca99f
lrush85 <lance.rush1@gmail.com>
2019-04-29 23:05:20 -0500

    Added routes for backend connection to front end for the vcard, updated logic for camera and verify-info added npm

--- client/src/components/verify/index.js
+++ client/src/components/verify/index.js
@@ -1,46 +1,86 @@
 import React, { Component } from 'react';
 import Dragula from "react-dragula";
+
 import "../../utils/googleAPI";
 import "./verify.css";
 
 
+
 export class VerifyInfo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labels: ["full name", "phone", "job title", "email", "fax"],
+    }
+
+    this.splitData = this.getInitialSplitData(this.props.data);
+  }
 
 
-  state = {
-    labels: ["full name", "phone", "job title", "email", "fax"],
+  getInitialSplitData = (data) => {    
+    console.log(data);
+    
+    return data.split("\n");
+  
   }
 
 dragulaDecorator = componentBackingInstance => {
   if (componentBackingInstance) {
   let options = {};
-  Dragula([componentBackingInstance], options);
+    const dragula = Dragula([componentBackingInstance], options);
+    dragula.on('drop', (itemThatWasDragged, parent, someOtherThing, itemBelow) => {
+      const updatedArray = [...parent.children].map((child) => child.children[0].value);
+      this.splitData = updatedArray;
+      // console.log();
+      // const keyOfItemDragged = itemThatWasDragged.value;
+      // const valueOfItemDragged = this.state.splitData[keyOfItemDragged];
+      // console.log(keyOfItemDragged);
+      // console.log(valueOfItemDragged);
+
+      // const updatedArray = [...this.state.splitData].filter((data, index) => index !== keyOfItemDragged);
+      // console.log(updatedArray);
+
+      // if (itemBelow) {
+      //   const newPosition = itemBelow.children[0].name - 1;
+      //   console.log('updated array', updatedArray);
+      //   // this.setState({ splitData: updatedArray });
+
+      // }
+      // // const positionOfItemBelow = this.state.splitData.findIndex(val => val === newPosition)
+      // const position = positionOfItemBelow - 1;
+      // console.log(newPosition, positionOfItemBelow, position, someOtherThing, parent);
+      // const listWithoutItem = this.st
+    })
   }
 }
 
 
 handleInputChange = event => {
 
-  let value = event.target.value;
+  let updatedValue = event.target.value;
   const name = event.target.name;
 
-  console.log(value);
-  console.log(name);
+  const updatedSplitData = this.state.splitData;
+  updatedSplitData[name] = updatedValue
 
   this.setState({
-    value: this.props.value
+    splitData: updatedSplitData
   });
 };
 
-  render() {
-    
-const data = this.props.data;
-console.log(data);
-let splitData = {...data.split("\n")};
-
-
+handleCreateContact = (event) => {
+  event.preventDefault();
+  const returnedContact = this.splitData;
+  console.log(this.splitData);
   
-    console.log(splitData);
+};
+
+
+
+  render() {
+
+    
     return (
       <div className="login-page">
         <h1>Verify Info</h1>
@@ -57,16 +97,17 @@ let splitData = {...data.split("\n")};
 
           <div className="container col" >
             <ul className="print" ref={this.dragulaDecorator}>
-              {Object.keys(splitData).map((key, item) =>(
+              {this.splitData.map((item, index) => (
                 
                 <li>
-                  <input key={item} name={item} defaultValue={splitData[key]} onChange={this.handleInputChange}/>
+                <input key={index} name={index} value={item} onChange={this.handleInputChange}/>
+
                 </li>
               ))}
             </ul>
           </div>
         </div>
-            <button>create contact</button>
+            <button onClick={this.handleCreateContact}>create contact</button>
             <br /><br />
             <button type="submit" formAction="/camera">retake</button>
           </form>
@@ -77,7 +118,10 @@ let splitData = {...data.split("\n")};
         </div>
       </div>
     )
+    
+    
   }
+  
 }
 
 export default VerifyInfo
