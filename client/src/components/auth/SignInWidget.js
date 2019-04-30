422aa71586498e287d05be774d5499284b532c1f
kupharmd05 <kupharmd05@gmail.com>
2019-04-29 23:02:21 -0500

    okta working with Google

--- client/src/components/auth/SignInWidget.js
+++ client/src/components/auth/SignInWidget.js
@@ -4,19 +4,33 @@ import OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 import '@okta/okta-signin-widget/dist/css/okta-theme.css';
 
- class SignInWidget extends Component {
+// https://dev-680655.okta.com/oauth2/v1/authorize?idp=0oaij9x751cV7zUvV356&client_id=0oaiiphavOO8bpeh2356&response_type=code&response_mode=fragment&scope=openid&redirect_uri=https://dev-680655.okta.com/oauth2/v1/authorize/callback&state=state-296bc9a0-a2a2-4a57-be1a-d0e2fd9bb601&nonce=foobar
+
+class SignInWidget extends Component {
   componentDidMount() {
     const el = ReactDOM.findDOMNode(this);
     this.widget = new OktaSignIn({
       baseUrl: this.props.baseUrl,
+      clientId: '0oaiiphavOO8bpeh2356',
       logo: 'CaptureMe.png',
-    //   idps: [
-    //       {type: 'GOOGLE', id: '0oaij9x751cV7zUvV356'},
-          
-    //   ]
-      
+      redirectUri: "http://localhost:3000/implicit/callback",
+      authParams: {
+        scope: [
+          'openid',
+          'email',
+          'profile',
+          'address',
+          'phone',
+          'groups'
+        ]
+      },
+      idpDisplay: 'PRIMARY',
+      idps: [
+        { type: 'GOOGLE', id: '0oaij9x751cV7zUvV356' }
+      ]
+
     });
-    this.widget.renderEl({el}, this.props.onSuccess, this.props.onError);
+    this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
   }
 
   componentWillUnmount() {
