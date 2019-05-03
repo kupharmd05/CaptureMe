f7425ab10d481f00e503b5c65cb67ba19b3960e1
lrush85 <lance.rush1@gmail.com>
2019-05-03 13:22:32 -0500

    updated vcard.js file?

--- controllers/vcard.js
+++ controllers/vcard.js
@@ -3,10 +3,10 @@ const db = require('../models');
 
 module.exports = {
   create (req, res) {
-        const dataThatIRequested = req.body.data;
-        const splitFirstWord = dataThatIRequested[0].split(' ');
+    const dataThatIRequested = req.body.data;
+    const splitFirstWord = dataThatIRequested[0].split(' ');
 
-        const vCard = vCardJS();
+    const vCard = vCardJS();
 
     vCard.firstName = splitFirstWord[0];
     vCard.lastName = splitFirstWord[1];
@@ -14,26 +14,26 @@ module.exports = {
     vCard.title = dataThatIRequested[2];
     vCard.email = dataThatIRequested[3];
 
-        // //save to file
-        // vCard.saveToFile('./eric-nesser.vcf');
+    // //save to file
+    // vCard.saveToFile('./eric-nesser.vcf');
 
-        // //get as formatted string
-        // console.log(vCard.getFormattedString());
-        //     },
-        db.VCard
-            .create(req.body)
-            .then(dbModel => {
-                // set content-type and disposition including desired filename
-                res.set('Content-Type', 'text/vcard; name="businesscard.vcf"');
-                res.set('Content-Disposition', 'inline; filename="businesscard.vcf"');
+    // //get as formatted string
+    // console.log(vCard.getFormattedString());
+    //     },
+    db.VCard
+      .create(req.body)
+      .then(dbModel => {
+        // set content-type and disposition including desired filename
+        res.set('Content-Type', 'text/vcard; name="businesscard.vcf"');
+        res.set('Content-Disposition', 'inline; filename="businesscard.vcf"');
 
-                // send the response
-                res.send(vCard.getFormattedString());
-            })
-            .catch(err => res.status(422).json(err));
+        // send the response
+        res.send(vCard.getFormattedString());
+      })
+      .catch(err => res.status(422).json(err));
 
 
 
-    },
+  },
 
 };
